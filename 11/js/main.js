5276061dfcc8d09d5527a0d93a3f19e165ff1b00
Keks <keks@htmlacademy.ru>
2023-04-09 21:57:19 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,17 @@
+import './form.js';
+import {renderGallery} from './full-photo.js';
+import { getData } from './api.js';
+import {debounce, showAlert} from './util.js';
+import {init, getFilterPictures} from './photo-filters.js';
+import './upload-photo.js';
+
+const debounceRenderGallery = debounce(renderGallery);
+
+getData()
+  .then((data) => {
+    init(data, debounceRenderGallery);
+    renderGallery(getFilterPictures());
+  })
+  .catch((err) => {
+    showAlert(err.message);
+  });
